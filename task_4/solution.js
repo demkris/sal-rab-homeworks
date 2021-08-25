d2c6769c718ce8d1489f2ac9a5a6fb3cb0249fd6
demkris <88244673+demkris@users.noreply.github.com>
2021-08-25 21:31:06 +0300

    Update solution.js

--- task_4/solution.js
+++ task_4/solution.js
@@ -6,13 +6,28 @@ function parseProducts(json) {
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
+    let data;
+    data =  JSON.parse(json);
+    let products;
+    products = data.products;
     // Верните как результат функции свойство products объекта data
+    return data.products;
 }
 
 // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
+function renderProductsCards(json)
+{
 // Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
+clearProducts();
+let products;
 // Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
+products = parseProducts(json);
 // Запишите в переменную length значение свойства products.length
+length = products.length;
 // Напишите цикл, в котором перебираете все products от 0 до (length - 1)
 // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
+for (let i = 0; i < length; i += 1) {
+    addProduct(products[i]);
+ }
+}
