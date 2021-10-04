b4c4d6c7ffcbcdfb65ac1044edc06abc65060186
demkris <88244673+demkris@users.noreply.github.com>
2021-10-04 12:44:05 +0300

    Update solution.js

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,21 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) 
-    {
-    let data = {client: {name, phone}, order: {address, sum}, goods: []};
-    data.client.name = name;
-    data.client.phone = phone;
-    address = "ул. " + street + ", дом " + house + ", " + entrance + " подъезд, " + floor + "этаж, кв " + flat;
+{
+    let data = {client: {}, order: {}, goods: []};
+    data.client = name + " " + phone; 
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв " + address.flat ;
     data.order.sum = sum;
     let countOfGoods = goods.length;
-    for (let i = 0; i < countOfGoods; i += 1) {
-        good = {
-            title: title,
-            count: count
-        };
-        data.goods.push(goods[i].good);
-    }
-    let jsonData = JSON.stringify({data});
+    for (let i = 0; i < countOfGoods; i += 1) 
+    {
+        data.goods.push
+        (
+            {title: goods[i].title, 
+             count: goods[i].count
+            }
+        );
+    }    
+    let jsonData = JSON.stringify({data: data});    
     return jsonData;
-}
+    }
